c27f7551696d554475d6f7abb9c827b440cd4866
GiUsEpPeP00 <113585432+GiUsEpPeP00@users.noreply.github.com>
2023-01-12 09:49:28 +0100

    First commit of login page
    First commit of a base login page, without grapichs and improvements

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log In</title>
+    </head>
+    <body>
+        <form action="verify.php" method="post">
+            <label for="Username">Username</label>
+            <input type="text" name="Username" id="Username" required>
+            <label for="Password">Password</label>
+            <input type="password" name="Password" id="Password" required>
+            <input type="submit" value="Log In">
+        </form>
+    </body>
+</html>
