961dd59eec93aaa19357dcdb2bdf2b2348c50158
GiUsEpPeP00 <113585432+GiUsEpPeP00@users.noreply.github.com>
2023-01-12 10:54:11 +0100

    Secondo commit della pagina di login
    Inserito il file che verifica la validità della password unito al file Json che contiene le password criptate. Ci sono due password diverse per Utente e Admin

--- login.php
+++ login.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Log In</title>
-    </head>
-    <body>
-        <form action="verify.php" method="post">
-            <label for="Username">Username</label>
-            <input type="text" name="Username" id="Username" required>
-            <label for="Password">Password</label>
-            <input type="password" name="Password" id="Password" required>
-            <input type="submit" value="Log In">
-        </form>
-    </body>
+
+<head>
+    <title>Log In</title>
+</head>
+
+<body>
+    <p>Inserisci la password per accedere al sondaggio</p>
+    <form action="verify.php" method="post">
+        <label for="Password">Password</label>
+        <input type="password" name="Password" id="Password" required>
+        <input type="submit" value="Log In">
+    </form>
+    <p style="color : red">
+        <?php
+        if (isset($_GET['passwordErrata'])) {
+            echo "Ppassword errata";
+        }
+        ?>
+    </p>
+</body>
+
 </html>
